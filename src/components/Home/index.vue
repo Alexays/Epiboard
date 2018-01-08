c46181117953bc58073fe63a58f4991edbe75ca8
Alexays <alexisr245@gmail.com>
2018-01-08 13:08:53 +0100

    feat: replace packery to muuri

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="container">
-        <div id="card-container">
-            <div v-for="card in cards" :key="card.name" :data-item-id="card.name" class="card">
-              <component :is="card" :key="card.name"></component>
-            </div>
-        </div>
+        <transition-group name="slide-fade" appear tag="div" id="card-container">
+            <component tag="div" v-for="card in cards" :data-item-id="card.name" class="card" :is="card" :key="card.name"></component>
+        </transition-group>
     </div>
 </template>
+<script src="../../../node_modules/web-animations-js/web-animations.min.js"></script>
+<script src="../../../node_modules/hammerjs/hammer.min.js"></script>
 <script src="./main.js"></script>
 <style lang="scss" rel='stylesheet/scss' src="./style.scss" scoped></style>
--- src/components/Home/main.js
+++ src/components/Home/main.js
@@ -1,67 +1,14 @@
 import _ from 'lodash';
-import Packery from 'packery';
-import Draggabilly from 'draggabilly';
+import Muuri from 'muuri';
 // Cards
 import System from '@/components/System';
 
 const cards = [
   System,
+  System,
 ];
 
-// get JSON-friendly data for items positions
-Packery.prototype.getShiftPositions = function getShiftPositions(attrName) {
-  const self = this;
-  return this.items.map(item => ({
-    attr: item.element.getAttribute(attrName || 'id'),
-    x: item.rect.x / self.packer.width,
-    y: item.rect.y,
-  }));
-};
-
-/*eslint-disable */
-Packery.prototype.initShiftLayout = function initShiftLayout(positions, attr) {
-  if (!positions) {
-    // Unhide cards
-    this.items
-      .filter(f => f)
-      .forEach((item) => {
-        item.element.style.display = 'block';
-      });
-    // if no initial positions, run packery layout
-    this.layout();
-    return;
-  }
-  // parse string to JSON
-  if (typeof positions === 'string') {
-    try {
-      positions = JSON.parse(positions);
-    } catch (err) {
-      throw err;
-    }
-  }
-  this._resetLayout();
-  // set item order and horizontal position from saved positions
-  this.items = positions.map(function map(itemPosition) {
-    const selector = '[' + (attr || 'id') + '="' + itemPosition.attr + '"]';
-    const itemElem = this.element.querySelector(selector);
-    if (itemElem) {
-      itemElem.style.display = 'block';
-      const item = this.getItem(itemElem);
-      if (!item) {
-        return false;
-      }
-      item.rect.x = Math.round(itemPosition.x * this.packer.width);
-      return item;
-    }
-    return false;
-  }, this)
-    .filter(value => value !== false);
-  this.shiftLayout();
-};
-/*eslint-enable */
-
 export default {
-
   name: 'Home',
   props: ['settings'],
   components: {},
@@ -77,36 +24,39 @@ export default {
   },
   mounted() {
     this.fetchCards();
-    const container = document.querySelector('#card-container');
     this.$nextTick(() => {
-      const pckry = new Packery(container, {
-        itemSelector: '.card',
-        columnWidth: 400,
-        gutter: 10,
-        percentPosition: false,
-        initLayout: false,
+      const grid = new Muuri('#card-container', {
+        items: '.card',
+        dragEnabled: true,
+        dragStartPredicate: {
+          handle: '.card-header',
+        },
+        layout: {
+          fillGaps: true,
+        },
+        dragSortInterval: 0,
       });
-      chrome.storage.sync.get('dragPositions', (initPositions) => {
-        let pos;
-        if (Object.keys(initPositions).length !== 0) {
-          pos = JSON.parse(initPositions.dragPositions);
-        }
-        // init layout with saved positions
-        pckry.initShiftLayout(pos, 'data-item-id');
-        const itemElems = pckry.getItemElements();
-        // make items draggable
-        for (let i = 0, len = itemElems.length; i < len; i += 1) {
-          const elem = itemElems[i];
-          elem.style.visibility = 'visible';
-          const draggie = new Draggabilly(elem, { handle: '.card-header' });
-          pckry.bindDraggabillyEvents(draggie);
-        }
-      });
+      // chrome.storage.sync.get('dragPositions', (initPositions) => {
+      //   let pos;
+      //   if (Object.keys(initPositions).length !== 0) {
+      //     pos = JSON.parse(initPositions.dragPositions);
+      //   }
+      //   // init layout with saved positions
+      //   pckry.initShiftLayout(pos, 'data-item-id');
+      //   const itemElems = pckry.getItemElements();
+      //   // make items draggable
+      //   for (let i = 0, len = itemElems.length; i < len; i += 1) {
+      //     const elem = itemElems[i];
+      //     const draggie = new Draggabilly(elem, { handle: '.card-header' });
+      //     pckry.bindDraggabillyEvents(draggie);
+      //     elem.style.visibility = 'visible';
+      //   }
+      // });
 
       // save drag positions
-      pckry.on('dragItemPositioned', () => {
-        chrome.storage.sync.set({ dragPositions: JSON.stringify(pckry.getShiftPositions('data-item-id')) });
-      });
+      // pckry.on('dragItemPositioned', () => {
+      //   chrome.storage.sync.set({ dragPositions: JSON.stringify(pckry.getShiftPositions('data-item-id')) });
+      // });
     });
   },
 };
