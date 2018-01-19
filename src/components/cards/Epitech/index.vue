eab36a8e12318b666d10a8b1859ea3e7ed1d351c
Alexays <alexisr245@gmail.com>
2018-01-19 11:16:01 +0100

    feat(wip): add epitech projects timeline

--- src/components/cards/Epitech/index.vue
+++ src/components/cards/Epitech/index.vue
@@ -3,13 +3,8 @@
     <v-card-title class="white--text">
       <span class="headline">Epitech</span>
     </v-card-title>
-    <v-card-text v-if="loading" class="white">
-      <v-progress-linear v-bind:indeterminate="true"></v-progress-linear>
-    </v-card-text>
-    <v-card-text v-if="!loading && !logged" class="white">
-      <h4>You're not logged !</h4>
-    </v-card-text>
-    <v-card-text v-if="!loading && logged" class="white">
+    <v-card-text v-if="logged" class="white">
+      <div v-if="!user.loading">
       <h3 class="subheading">
         {{user.title}} 
         <v-chip label>{{user.gpa[0].gpa}}</v-chip>
@@ -18,6 +13,19 @@
         {{user.studentyear}}<sup>{{user.studentyear > 1 ? 'nd' : 'st'}}</sup> year,
          Promo {{user.promo}}
       </p>
+      </div>
+      <div v-if="!projects.loading">
+        <div class="p-timeline" v-for="(project, key) in projects" :key="key">
+          <h4>{{project.title}}</h4>
+          <v-progress-linear height="6" :value="project.timeline_barre"></v-progress-linear>
+        </div>
+      </div>
+    </v-card-text>
+    <v-card-text v-if="user.loading || projects.loading" class="white">
+      <v-progress-linear v-bind:indeterminate="true"></v-progress-linear>
+    </v-card-text>
+    <v-card-text v-if="!user.loading && !logged" class="white">
+      <h4>You're not logged !</h4>
     </v-card-text>
 </div>
 </template>
--- src/components/cards/Epitech/main.js
+++ src/components/cards/Epitech/main.js
@@ -6,9 +6,13 @@ export default {
   components: {},
   data() {
     return {
-      loading: true,
       logged: true,
-      user: {},
+      user: {
+        loading: true,
+      },
+      projects: {
+        loading: true,
+      },
     };
   },
   methods: {
@@ -16,13 +20,11 @@ export default {
       chrome.cookies.get({ url: API, name: 'user' }, (cookie) => {
         if (chrome.runtime.lastError) {
           this.logged = false;
-          this.loading = false;
           return;
         }
         const date = new Date(cookie.expirationDate * 1000);
         if (date < new Date()) {
           this.logged = false;
-          this.loading = false;
           return;
         }
       });
@@ -30,17 +32,32 @@ export default {
     getUserInfo() {
       this.axios.get(API + '/user/?format=json').then((response) => {
         if (!response.data) {
-          this.logged = false;
-          this.loading = false;
+          this.user.loading = false;
           return;
         }
-        this.loading = false;
         this.user = response.data;
+        this.user.loading = false;
+      });
+    },
+    getProjects() {
+      this.axios.get(API + '/?format=json').then((response) => {
+        console.log(response);
+        if (!response.data) {
+          this.projects.loading = false;
+          return;
+        }
+        this.projects = response.data.board.projets
+          .filter(f => f.timeline_barre < 100)
+          .sort((a, b) => {
+            const [aDate, aTime] = a.timeline_start.split(', ');
+          });
+        this.projects.loading = false;
       });
     },
   },
   mounted() {
     this.getCookie();
     this.getUserInfo();
+    this.getProjects();
   },
 };
