6a11a8f853780dd9ed1c83a7e3689c1875d7d8e1
Alexis <alexisr245@gmail.com>
2018-01-26 13:37:11 +0100

    refactor: useless relative path

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,4 +1,4 @@
-const files = require.context('../cards', true, /^\.\/\w+\/index\.vue$/);
+const files = require.context('./', true, /^\.\/\w+\/index\.vue$/);
 const modules = {};
 files.keys().forEach((key) => {
   const moduleName = key.replace(/(\.\/|\.js)/g, '');
