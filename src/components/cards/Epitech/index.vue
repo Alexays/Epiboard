5fe9714708e754199c3b9e17e23c4ba647ca6539
Alexis <alexisr245@gmail.com>
2018-02-19 18:28:51 +0100

    feat: add students registered

--- src/components/cards/Epitech/index.vue
+++ src/components/cards/Epitech/index.vue
@@ -57,9 +57,9 @@
                     <v-list-tile-title v-html="room.room.name"></v-list-tile-title>
                     <v-list-tile-sub-title v-html="room.acti_title + '<br/>' + room.dateString"></v-list-tile-sub-title>
                   </v-list-tile-content>
-                  <v-list-tile-action :title="room.room.seats + ' seats'">
+                  <v-list-tile-action :title="room.total_students_registered + ' student(s) for ' + room.room.seats + ' seats'">
                     <v-btn icon dark color="blue">
-                      {{room.room.seats}}
+                      {{room.total_students_registered}}/{{room.room.seats}}
                     </v-btn>
                   </v-list-tile-action>
                 </v-list-tile>
