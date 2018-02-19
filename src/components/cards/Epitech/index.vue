79015cbbdc650ff911125a25bf486b6f37eeb0db
Alexis <alexisr245@gmail.com>
2018-02-19 16:44:48 +0100

    feat(epitech): occuped rooms

--- src/components/cards/Epitech/index.vue
+++ src/components/cards/Epitech/index.vue
@@ -1,46 +1,77 @@
 <template>
   <div id="epitech">
-    <div v-if="!user.loading && user.loaded" class="text-xs-center">
-      <h3>
-        {{user.title}}
-        <br>
-        <v-chip label disabled>{{user.gpa[0].gpa}}
-          <small>&nbsp;G.P.A.</small>
-        </v-chip>
-        <v-chip label disabled>{{user.credits}}
-          <small>&nbsp;Credits</small>
-        </v-chip>
-      </h3>
-      <p>
-        {{user.studentyear}}
-        <sup>{{user.studentyear > 1 ? 'nd' : 'st'}}</sup> year, Promo {{user.promo}}
-      </p>
-    </div>
-    <div class="project" v-if="!projects.loading && projects.loaded">
-      <div class="p-timeline" v-for="(project, key) in projects.data" :key="key">
-        <a target="_blank" v-bind:href="API + project.title_link">
-          <h4>{{project.title}}</h4>
-        </a>
-        <small>{{project.timeline_start}}</small>
-        <small class="end_date">{{project.timeline_end}}</small>
-        <div v-if="project.timeline_barre < 60">
-          <v-progress-linear height="6" :value="project.timeline_barre"></v-progress-linear>
-        </div>
-        <div v-if="project.timeline_barre >= 60 && project.timeline_barre < 80">
-          <v-progress-linear color="orange" background-color="orange lighten-2" height="6" :value="project.timeline_barre"></v-progress-linear>
-        </div>
-        <div v-if="project.timeline_barre >= 80">
-          <v-progress-linear color="red" background-color="red lighten-2" height="6" :value="project.timeline_barre"></v-progress-linear>
-        </div>
-      </div>
-    </div>
-    <div v-if="user.loading || projects.loading" class="white">
-      <v-progress-linear v-bind:indeterminate="true"></v-progress-linear>
-    </div>
-    <div v-if="!user.loading && !projects.loading && !user.loaded && !projects.loaded" class="white text-xs-center">
+    <div v-if="!is_logged" class="white text-xs-center">
       <i class="material-icons md-48">error_outline</i>
       <h2 class="subheading">You must be logged to epitech to use this card.</h2>
     </div>
+    <v-tabs v-else dark grow show-arrows color="blue-grey" class="no-margins">
+      <v-tabs-slider color="white"></v-tabs-slider>
+      <v-tab>Infos</v-tab>
+      <v-tab>Ocupped Rooms</v-tab>
+      <v-tabs-items>
+        <v-tab-item>
+          <div v-if="!user.loading" class="text-xs-center">
+            <h3>
+              {{user.title}}
+              <br>
+              <v-chip label disabled>{{user.gpa[0].gpa}}
+                <small>&nbsp;G.P.A.</small>
+              </v-chip>
+              <v-chip label disabled>{{user.credits}}
+                <small>&nbsp;Credits</small>
+              </v-chip>
+            </h3>
+            <p>
+              {{user.studentyear}}
+              <sup>{{user.studentyear > 1 ? 'nd' : 'st'}}</sup> year, Promo {{user.promo}}
+            </p>
+          </div>
+          <div v-if="!projects.loading" class="project">
+            <div class="p-timeline" v-for="(project, key) in projects.data" :key="key">
+              <a target="_blank" v-bind:href="API + project.title_link">
+                <h4>{{project.title}}</h4>
+              </a>
+              <small>{{project.timeline_start}}</small>
+              <small class="end_date">{{project.timeline_end}}</small>
+              <div v-if="project.timeline_barre < 60">
+                <v-progress-linear height="6" :value="project.timeline_barre"></v-progress-linear>
+              </div>
+              <div v-if="project.timeline_barre >= 60 && project.timeline_barre < 80">
+                <v-progress-linear color="orange" background-color="orange lighten-2" height="6" :value="project.timeline_barre"></v-progress-linear>
+              </div>
+              <div v-if="project.timeline_barre >= 80">
+                <v-progress-linear color="red" background-color="red lighten-2" height="6" :value="project.timeline_barre"></v-progress-linear>
+              </div>
+            </div>
+          </div>
+          <div v-if="projects.loading || user.loading" class="white">
+            <v-progress-linear v-bind:indeterminate="true"></v-progress-linear>
+          </div>
+        </v-tab-item>
+        <v-tab-item>
+          <div>
+            <v-list v-if="!rooms.loading" three-line dense>
+              <template v-for="room of rooms.data">
+                <v-list-tile :key="room">
+                  <v-list-tile-content>
+                    <v-list-tile-title v-html="room.room.name"></v-list-tile-title>
+                    <v-list-tile-sub-title v-html="room.acti_title + '<br/>' + room.dateString"></v-list-tile-sub-title>
+                  </v-list-tile-content>
+                  <v-list-tile-action :title="room.room.seats + ' seats'">
+                    <v-btn icon dark color="blue">
+                      {{room.room.seats}}
+                    </v-btn>
+                  </v-list-tile-action>
+                </v-list-tile>
+              </template>
+            </v-list>
+            <div v-if="rooms.loading" class="white">
+              <v-progress-linear v-bind:indeterminate="true"></v-progress-linear>
+            </div>
+          </div>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-tabs>
   </div>
 </template>
 <script src="./main.js"></script>
--- src/components/cards/Epitech/main.js
+++ src/components/cards/Epitech/main.js
@@ -5,18 +5,26 @@ export default {
   data() {
     return {
       API: 'https://intra.epitech.eu',
+      is_logged: true,
+      location: null,
       user: {
-        loaded: false,
         loading: true,
       },
       projects: {
         data: [],
-        loaded: false,
+        loading: true,
+      },
+      rooms: {
+        data: [],
         loading: true,
       },
     };
   },
   methods: {
+    parseCalendarDate(epiDate) {
+      const date = epiDate.replace(' ', '-').replace(/:/g, '-').split('-');
+      return new Date(date[0], date[1] - 1, date[2], date[3], date[4]);
+    },
     parseDate(epiDate) {
       const date = epiDate.replace(', ', '/').replace(':', '/').replace('h', '/').split('/');
       return new Date(date[2], date[1] - 1, date[0], date[3], date[4]);
@@ -26,8 +34,10 @@ export default {
         .then((response) => {
           if (!response.data) return;
           Object.assign(this.user, response.data);
-          this.user.loaded = true;
-        }).finally(() => {
+        }).catch(() => {
+          this.is_logged = false;
+        })
+        .finally(() => {
           this.user.loading = false;
         });
     },
@@ -39,6 +49,7 @@ export default {
       return this.axios.get(`${this.API}/?format=json`)
         .then((response) => {
           if (!response.data) return Promise.resolve();
+          this.location = response.data.infos.location;
           this.projects.data = response.data.board.projets
             .filter(async f => f.timeline_barre < 100 &&
               !f.date_inscription && this.parseDate(f.timeline_start) <= new Date());
@@ -51,15 +62,36 @@ export default {
           this.projects.data = res.filter(f => f.isRegistered)
             .sort((a, b) =>
               this.parseDate(a.timeline_end) - this.parseDate(b.timeline_end));
-          this.projects.loaded = true;
+        }).catch(() => {
+          this.is_logged = false;
         })
         .finally(() => {
           this.projects.loading = false;
         });
     },
+    getRoom() {
+      const d = new Date();
+      const dString = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+      return this.axios.get(`${this.API}/planning/load?format=json&start=${dString}&end=${dString}`)
+        .then((res) => {
+          this.rooms.data = res.data.filter(f => f.instance_location === this.location && f.room)
+            .map((f) => {
+              f.room.name = f.room.code.substring(f.room.code.lastIndexOf('/') + 1);
+              f.start = this.parseCalendarDate(f.start);
+              f.end = this.parseCalendarDate(f.end);
+              f.dateString = `Taken from <b>${f.start.getHours()}h${(`0${f.start.getMinutes()}`).substr(-2)}</b>
+              to <b>${f.end.getHours()}h${(`0${f.end.getMinutes()}`).substr(-2)}</b>`;
+              return f;
+            }).filter(f => f.end > new Date())
+            .sort((a, b) => a.start - b.start);
+        }).finally(() => {
+          this.rooms.loading = false;
+        });
+    },
   },
   mounted() {
     Promise.all([this.getUserInfo(), this.getProjects()])
+      .then(() => this.getRoom())
       .finally(() => this.$emit('init'));
   },
 };
