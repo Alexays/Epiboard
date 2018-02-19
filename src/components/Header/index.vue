760f7f62bbb715d5eb074ce99176515b34240646
Alexis <alexisr245@gmail.com>
2018-02-19 12:23:37 +0100

    refactor: blur header background on load

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,11 +1,10 @@
 <template>
   <header tag="header" class="grey">
-    <div class="background" v-if="background" v-lazy:background-image="background">
-      <a :href="current.length > 0 ? 'https://www.google.com/#q=' + current : null" v-show="$route.path === '/'">
-        <vue-typer tag="h1" :text="messages" pre-erase-delay='5000' erase-delay='250' :shuffle='true' @erased='addTrends' @typed='onTyped'></vue-typer>
-      </a>
-      <vue-typer v-if="$route.path !== '/'" tag="h1" :text="$route.name" :repeat='0'></vue-typer>
-    </div>
+    <img class="background" v-if="background" v-lazy="background" />
+    <a :href="current.length > 0 ? 'https://www.google.com/#q=' + current : null" v-show="$route.path === '/'">
+      <vue-typer tag="h1" :text="messages" pre-erase-delay='5000' erase-delay='250' :shuffle='true' @erased='addTrends' @typed='onTyped'></vue-typer>
+    </a>
+    <vue-typer v-if="$route.path !== '/'" tag="h1" :text="$route.name" :repeat='0'></vue-typer>
     <v-btn v-if="$route.path === '/settings'" id="settings" flat icon color="white" to="/">
       <v-icon x-large>arrow_back</v-icon>
     </v-btn>
