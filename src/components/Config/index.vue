207044e8e7de81c0dc8a38ba823d6352d77b44c3
Alexis <alexisr245@gmail.com>
2018-03-25 12:04:45 +0200

    style: align

--- src/components/Config/index.vue
+++ src/components/Config/index.vue
@@ -3,10 +3,7 @@
     <div v-for="config in settings" :key="config.title">
       <h4 class="headline">{{config.title}}</h4>
       <v-select v-if="config.type === 'select'" v-bind:items="config.items" v-model="config.value" :label="config.label || ''" autocomplete></v-select>
-      <v-switch v-if="config.type === 'switch'"
-      :label="config.label || ''"
-      v-model="config.value"
-    ></v-switch>
+      <v-switch v-if="config.type === 'switch'" :label="config.label || ''" v-model="config.value"></v-switch>
     </div>
     <v-btn v-on:click="save()" color="primary">Save</v-btn>
   </div>
