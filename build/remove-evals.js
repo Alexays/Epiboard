ffca2614763d03612853de558bf7fce5be1c3a60
Alexis <alexisr245@gmail.com>
2018-04-24 21:17:29 +0200

    refactor: remove-eval script

--- build/remove-evals.js
+++ build/remove-evals.js
@@ -2,30 +2,24 @@
 
 const path = require('path');
 const fs = require('fs');
+const glob = require('glob');
 
 const BUNDLE_DIR = path.join(__dirname, '../dist/static/js');
-const bundles = [
-  'vendor.js',
-  'app.js',
-  'manifest.js'
-];
 
 const evalRegexForProduction = /;([a-z])=function\(\){return this}\(\);try{\1=\1\|\|Function\("return this"\)\(\)\|\|\(0,eval\)\("this"\)}catch\(t\){"object"==typeof window&&\(\1=window\)}/g;
 const evalRegexForDevelopment = /;\s*\/\/ This works in non-strict mode\s*([a-z])\s*=\s*\(\s*function\(\)\s*\{\s*return this;\s*}\)\(\);\s*try\s*{\s*\/\/\s*This works if eval is allowed(?:\s*|.+){1,14}/g;
 
 const removeEvals = (file) => {
-  console.info(`Removing eval() from ${file}`);
-
   return new Promise((resolve, reject) => {
     fs.readFile(file, 'utf8', (err, data) => {
-      if(err) {
+      if (err) {
         reject(err);
         return;
       }
 
       const regex = process.env.NODE_ENV === 'production' ? evalRegexForProduction : evalRegexForDevelopment;
 
-      if(!regex.test(data)) {
+      if (!regex.test(data)) {
         reject(`No CSP specific code found in ${file}.`);
         return;
       }
@@ -33,7 +27,7 @@ const removeEvals = (file) => {
       data = data.replace(regex, '=window;');
 
       fs.writeFile(file, data, (err) => {
-        if(err) {
+        if (err) {
           reject(err);
           return;
         }
@@ -45,10 +39,12 @@ const removeEvals = (file) => {
 };
 
 const main = () => {
-  bundles.forEach(bundle => {
-    removeEvals(path.join(BUNDLE_DIR, bundle))
-      .then(() => console.info(`Bundle ${bundle}: OK`))
-      .catch(console.error);
+  glob(`${BUNDLE_DIR}/*.js`, {}, (er, files) => {
+    for (let i = 0; i < files.length; i += 1) {
+      removeEvals(files[i])
+        .then(() => console.info(`Bundle ${files[i]}: OK`))
+        .catch(console.error);
+    }
   });
 };
 
