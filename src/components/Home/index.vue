dd615ab3970fe5663c60284e40b49984c58a3fd1
Alexis <alexisr245@gmail.com>
2018-04-25 22:56:10 +0200

    style: z-index fab

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="home">
     <transition name="fab-transition" mode="out-in">
-      <v-speed-dial v-show="showFab" v-model="fab" :top="true" :right="true" direction="bottom" transition="slide-y-transition">
+      <v-speed-dial v-show="showFab" v-model="fab" top right direction="bottom" transition="slide-y-transition">
         <v-btn v-model="fab" slot="activator" dark fab color="blue">
           <v-icon>add</v-icon>
           <v-icon>close</v-icon>
         </v-btn>
-        <v-btn :title="key" v-for="(value, key) in availableCards" :key="key" v-on:click="addCard(key, value)" dark
-          small color="green">
-          <span>{{key}}</span>
+        <v-btn :title="key" v-for="(value, key) in availableCards" :key="key" v-on:click="addCard(key, value)" dark small color="green">
+          {{key}}
         </v-btn>
       </v-speed-dial>
     </transition>
