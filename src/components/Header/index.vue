c13b47c6d0f9d699ce1926f3d9e9b4f8d7c27f8b
Alexis <alexisr245@gmail.com>
2018-05-25 22:23:11 +0200

    style: remove useless styles

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -6,7 +6,7 @@
       <v-btn title="Home" v-if="$route.path === '/settings'" id="settings" flat icon color="white" to="/">
         <v-icon x-large>&#xE5C4;</v-icon>
       </v-btn>
-      <v-btn title="Settings" v-show="$route.path !== '/settings'" id="settings" flat icon color="white" to="/settings">
+      <v-btn title="Settings" v-else id="settings" flat icon color="white" to="/settings">
         <v-icon>&#xE5D4;</v-icon>
       </v-btn>
     </template>
@@ -16,14 +16,19 @@
       </v-btn>
       <vue-typer text-field :text="$route.path === '/' ? messages : [$route.name]" full-erase :erase-delay="5000" :repeat="messages.length === 1 || $route.path !== '/' ? 0 : Infinity"></vue-typer>
       <v-spacer></v-spacer>
-      <v-btn title="Settings" v-show="$route.path !== '/settings'" to="/settings" icon>
-        <v-icon>&#xE5D4;</v-icon>
-      </v-btn>
       <v-btn title="Home" v-if="$route.path === '/settings'" to="/" icon>
         <v-icon>&#xE5C4;</v-icon>
+      </v-btn>
+      <v-btn title="Settings" v-else to="/settings" icon>
+        <v-icon>&#xE5D4;</v-icon>
       </v-btn>
     </v-toolbar>
   </header>
 </template>
 <script src="./main.js"></script>
 <style lang="scss" rel='stylesheet/scss' src="./style.scss" scoped></style>
+<style>
+.progressive-image-preloader {
+  background: linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.35));
+}
+</style>
