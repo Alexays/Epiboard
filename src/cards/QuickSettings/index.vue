e0d3bd0f03ca2cc8abe32125f7234dce58a64d98
Alexis <alexisr245@gmail.com>
2018-05-26 18:48:41 +0200

    style: compact quick settings

--- src/cards/QuickSettings/index.vue
+++ src/cards/QuickSettings/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="quick-settings">
-    <v-checkbox label="Cache" v-model="types.cache" color="primary" hide-details></v-checkbox>
-    <v-checkbox label="Cookies" v-model="types.cookies" color="primary" hide-details></v-checkbox>
-    <v-checkbox label="History" v-model="types.history" color="primary" hide-details></v-checkbox>
-    <v-checkbox label="Local Storage" v-model="types.localStorage" color="primary" hide-details></v-checkbox>
-    <v-btn :disabled="isFalse() || loading" v-on:click="clear()" flat>{{ loading ? 'Clearing' : 'Clear' }}</v-btn>
-  </div>
+  <v-layout id="quick-settings">
+    <v-flex>
+      <v-checkbox label="Cache" v-model="types.cache" color="primary" hide-details></v-checkbox>
+      <v-checkbox label="Cookies" v-model="types.cookies" color="primary" hide-details></v-checkbox>
+      <v-checkbox label="History" v-model="types.history" color="primary" hide-details></v-checkbox>
+      <v-checkbox label="Local Storage" v-model="types.localStorage" color="primary" hide-details></v-checkbox>
+    </v-flex>
+    <v-layout align-end justify-end>
+      <v-btn :disabled="isFalse() || loading" v-on:click="clear()" flat>{{ loading ? 'Clearing' : 'Clear' }}</v-btn>
+    </v-layout>
+  </v-layout>
 </template>
 <script src="./main.js"></script>
 <style lang="scss" rel='stylesheet/scss' src="./style.scss" scoped></style>
