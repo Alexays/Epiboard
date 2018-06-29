fa9661174a84dcec13f159e430d3ece583e87512
Alexis <alexisr245@gmail.com>
2018-06-29 13:18:53 +0200

    chore: v1.4.9

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -95,9 +95,6 @@ const webpackConfig = merge(baseWebpackConfig, {
       // necessary to consistently work with multiple chunks
       chunksSortMode: 'dependency'
     }),
-    // keep module.id stable when vendor modules does not change
-    new webpack.NamedChunksPlugin(),
-    new webpack.HashedModuleIdsPlugin(),
     // copy custom static assets
     new CopyWebpackPlugin([{
         from: path.resolve(__dirname, '../static'),
@@ -140,6 +137,8 @@ const webpackConfig = merge(baseWebpackConfig, {
         },
       },
     },
+    namedChunks: true,
+    hashedModuleIds: true,
     runtimeChunk: 'single',
     minimizer: [
       new UglifyJsPlugin({
