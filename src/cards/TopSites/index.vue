1da7cceb6cf2942e753153809d8b6859c829e8f9
Alexis <alexisr245@gmail.com>
2018-07-04 20:37:32 +0200

    style(topSites): div instead of ul

--- src/cards/TopSites/index.vue
+++ src/cards/TopSites/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card-text id="top-sites">
-    <ul v-if="topSites.length">
+    <div v-if="topSites.length">
       <li v-for="node in topSites" :key="node.url">
         <a :href="node.url" class="text--primary">
           <i v-if="node.icon" :style="{'background-image': 'url('+node.icon+')'}"/>
@@ -8,7 +8,7 @@
           <span class="n-title">{{ node.title || node.url }}</span>
         </a>
       </li>
-    </ul>
+    </div>
     <div v-else class="text-xs-center">
       <v-icon x-large>find_in_page</v-icon>
       <h2 class="subheading">You have no top sites.</h2>
