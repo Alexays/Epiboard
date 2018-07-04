4f5c94b43387179fbf110c01621b639eaf5cc801
Alexis <alexisr245@gmail.com>
2018-07-04 18:41:10 +0200

    feat(topSites): linter

--- src/cards/TopSites/index.vue
+++ src/cards/TopSites/index.vue
@@ -1,12 +1,14 @@
 <template>
   <v-card-text id="top-sites">
-    <li v-if="topSites.length" v-for="node in topSites" :key="node.url">
-      <a :href="node.url" class="text--primary">
-        <i v-if="node.icon" :style="{'background-image': 'url('+node.icon+')'}"/>
-        <v-icon v-else x-large>insert_drive_file</v-icon>
-        <span class="n-title">{{ node.title || node.url }}</span>
-      </a>
-    </li>
+    <ul v-if="topSites.length">
+      <li v-for="node in topSites" :key="node.url">
+        <a :href="node.url" class="text--primary">
+          <i v-if="node.icon" :style="{'background-image': 'url('+node.icon+')'}"/>
+          <v-icon v-else x-large>insert_drive_file</v-icon>
+          <span class="n-title">{{ node.title || node.url }}</span>
+        </a>
+      </li>
+    </ul>
     <div v-else class="text-xs-center">
       <v-icon x-large>find_in_page</v-icon>
       <h2 class="subheading">You have no top sites.</h2>
