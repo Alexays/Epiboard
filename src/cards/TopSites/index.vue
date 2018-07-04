e435878cb2496a4e65b33b6376098cf1f8a24b63
Alexis <alexisr245@gmail.com>
2018-07-04 17:18:04 +0200

    feat(topSites): no top sites message

--- src/cards/TopSites/index.vue
+++ src/cards/TopSites/index.vue
@@ -1,12 +1,16 @@
 <template>
   <v-card-text id="top-sites">
-    <li v-for="node in topSites" :key="node.url">
+    <li v-if="topSites.length" v-for="node in topSites" :key="node.url">
       <a :href="node.url" class="text--primary">
         <i v-if="node.icon" :style="{'background-image': 'url('+node.icon+')'}"/>
         <v-icon v-else x-large>insert_drive_file</v-icon>
         <span class="n-title">{{ node.title || node.url }}</span>
       </a>
     </li>
+    <div v-else class="text-xs-center">
+      <v-icon x-large>find_in_page</v-icon>
+      <h2 class="subheading">You have no top sites.</h2>
+    </div>
   </v-card-text>
 </template>
 <script src="./main.js"></script>
