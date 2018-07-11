1585d3e83f37e409f3106a4c7b0df584724b574b
Alexis <alexisr245@gmail.com>
2018-07-11 18:14:56 +0200

    fix: linter

--- src/cards/LastFm/api.js
+++ src/cards/LastFm/api.js
@@ -4,34 +4,28 @@ const API = 'http://ws.audioscrobbler.com/2.0/?format=json';
 const fallback = 'https://lastfm-img2.akamaized.net/i/u/300x300/c6f59c1e5e7240a4c0d427abd71f3dbb';
 
 export default {
-  parseImage(image) {
-    return {
-      small: image[0]['#text'] !== '' ? image[0]['#text'] : fallback,
-      medium: image[1]['#text'] !== '' ? image[1]['#text'] : fallback,
-      large: image[2]['#text'] !== '' ? image[2]['#text'] : fallback,
-      extralarge: image[3]['#text'] !== '' ? image[3]['#text'] : fallback,
-    };
+  parseRes(res) {
+    return res.map((f) => {
+      f.image = {
+        small: f.image[0]['#text'] !== '' ? f.image[0]['#text'] : fallback,
+        medium: f.image[1]['#text'] !== '' ? f.image[1]['#text'] : fallback,
+        large: f.image[2]['#text'] !== '' ? f.image[2]['#text'] : fallback,
+        extralarge: f.image[3]['#text'] !== '' ? f.image[3]['#text'] : fallback,
+      };
+      return f;
+    });
   },
   getTopArtists(apiKey, user, limit, period) {
     return axios.get(`${API}&method=user.gettopartists&api_key=${apiKey}&user=${user}&limit=${limit}&period=${period}`)
-      .then(res => res.data.topartists.artist.map((f) => {
-        f.image = this.parseImage(f.image);
-        return f;
-      }));
+      .then(res => this.parseRes(res.data.topartists.artist));
   },
   getTopAlbums(apiKey, user, limit, period) {
     return axios.get(`${API}&method=user.gettopalbums&api_key=${apiKey}&user=${user}&limit=${limit}&period=${period}`)
-      .then(res => res.data.topalbums.album.map((f) => {
-        f.image = this.parseImage(f.image);
-        return f;
-      }));
+      .then(res => this.parseRes(res.data.topalbums.album));
   },
   getTopTracks(apiKey, user, limit, period) {
     return axios.get(`${API}&method=user.gettoptracks&api_key=${apiKey}&user=${user}&limit=${limit}&period=${period}`)
-      .then(res => res.data.toptracks.track.map((f) => {
-        f.image = this.parseImage(f.image);
-        return f;
-      }));
+      .then(res => this.parseRes(res.data.toptracks.track));
   },
   getRecentTracks(apiKey, user, limit) {
     return axios.get(`${API}&method=user.getrecenttracks&api_key=${apiKey}&user=${user}&limit=${limit}`)
