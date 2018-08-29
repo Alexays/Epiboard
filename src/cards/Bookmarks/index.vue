4209758491facaedfdc0f34c2544f3a9f7127c2d
Alexis <alexisr245@gmail.com>
2018-08-29 15:01:23 +0200

    feat(List): add virtual-scroller
    style: fix linter issues
    
    chore: update vuetify
    
    style: fix vuetify update

--- src/cards/Bookmarks/index.vue
+++ src/cards/Bookmarks/index.vue
@@ -1,9 +1,10 @@
 <template>
   <v-tabs
+    id="bookmarks"
     v-model="active"
     :dark="!$store.state.settings.theme.light"
     :light="$store.state.settings.theme.light"
-    slider-color="foreground" color="primary" id="bookmarks" grow show-arrows>
+    slider-color="foreground" color="primary" grow show-arrows>
     <v-tab><span v-t="'Bookmarks.recents'"/></v-tab>
     <v-tab><span v-t="'Bookmarks.all'"/></v-tab>
     <v-tab v-for="tab in folders" :key="tab.id">{{ tab.name }}</v-tab>
@@ -17,29 +18,40 @@
               v-t="'Bookmarks.back_parent'"
               v-if="tab.parentNode" class="body-2" small @click="backParent(tab)"/>
           </div>
-          <list v-else :data="tab.data" icon>
-            <template slot="icon" slot-scope="{item}">
-              <img
-                v-if="item.url && $utils.getFavicon(item.url)"
-                :src="$utils.getFavicon(item.url)">
-              <v-icon v-else-if="item.url">insert_drive_file</v-icon>
-              <v-icon v-else>folder</v-icon>
-            </template>
-            <template slot="action" slot-scope="{item}">
-              <templace v-if="item.url">
-                {{ new Date(item.dateAdded)
-                .toLocaleDateString($i18n.locale, $options.dateOption) }}
-              </templace>
-              <v-icon
-                v-else-if="foldersId.indexOf(item.id) === -1"
-                :title="$t('Bookmarks.add_folder')" @click="addTab(item)">
-                add
-              </v-icon>
-              <v-icon v-else :title="$t('Bookmarks.remove_folder')" @click="removeTab(item)">
-                delete
-              </v-icon>
-            </template>
-          </list>
+          <template v-else>
+            <v-btn v-if="tab.parentNode" small flat block @click="backParent(tab)">
+              <v-icon>arrow_back</v-icon>
+              <span v-t="'Bookmarks.back_parent'"/>
+            </v-btn>
+            <list :data="tab.data" icon>
+              <template slot="icon" slot-scope="{item}">
+                <img
+                  v-if="item.url && $utils.getFavicon(item.url)"
+                  :src="$utils.getFavicon(item.url)">
+                <v-icon v-else-if="item.url">insert_drive_file</v-icon>
+                <v-icon v-else>folder</v-icon>
+              </template>
+              <template slot="content" slot-scope="{item}">
+                <div @click="getSubFolder(tab, item)">
+                  {{ item.title && item.title.length ? item.title : item.url }}
+                </div>
+              </template>
+              <template slot="action" slot-scope="{item}">
+                <templace v-if="item.url">
+                  {{ new Date(item.dateAdded)
+                  .toLocaleDateString($i18n.locale, $options.dateOption) }}
+                </templace>
+                <v-icon
+                  v-else-if="foldersId.indexOf(item.id) === -1"
+                  :title="$t('Bookmarks.add_folder')" @click="addTab(item)">
+                  add
+                </v-icon>
+                <v-icon v-else :title="$t('Bookmarks.remove_folder')" @click="removeTab(item)">
+                  delete
+                </v-icon>
+              </template>
+            </list>
+          </template>
         </v-card-text>
       </v-tab-item>
     </v-tabs-items>
--- src/components/List/index.vue
+++ src/components/List/index.vue
@@ -1,38 +1,46 @@
 <template>
   <v-list dense>
-    <v-list-tile v-for="item in data" :key="item.id" :href="item.url">
-      <v-list-tile-avatar :size="16">
-        <slot :item="item" name="icon">
-          <img v-if="item.icon" :src="item.icon">
-          <v-icon v-else-if="icon">insert_drive_file</v-icon>
-        </slot>
-      </v-list-tile-avatar>
-      <v-list-tile-content :title="item.url" class="caption">
-        <v-list-tile-sub-title v-if="item.title && item.title.length">
-          {{ item.title }}
-        </v-list-tile-sub-title>
-        <v-list-tile-sub-title v-else>{{ item.url }}</v-list-tile-sub-title>
-      </v-list-tile-content>
-      <v-list-tile-action>
-        <v-list-tile-action-text>
-          <slot :item="item" name="action">
-            <span v-if="item.date">
-              {{ item.date.toLocaleDateString($i18n.locale, $options.dateOption) }}
-            </span>
-          </slot>
-        </v-list-tile-action-text>
-      </v-list-tile-action>
-    </v-list-tile>
+    <virtual-scroller :items="data" :item-height="34" class="scroller">
+      <template scope="{item}">
+        <!-- TODO: @click with empty callback is needed to have hover effect -->
+        <v-list-tile :href="item.url" @click="() => {}">
+          <v-list-tile-avatar :size="16">
+            <slot :item="item" name="icon">
+              <img v-if="item.icon" :src="item.icon">
+              <v-icon v-else-if="icon">insert_drive_file</v-icon>
+            </slot>
+          </v-list-tile-avatar>
+          <v-list-tile-content :title="item.url" class="caption">
+            <v-list-tile-sub-title>
+              <slot :item="item" name="content">
+                {{ item.title && item.title.length ? item.title : item.url }}
+              </slot>
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-list-tile-action-text>
+              <slot :item="item" name="action">
+                <span v-if="item.date">
+                  {{ item.date.toLocaleDateString($i18n.locale, $options.dateOption) }}
+                </span>
+              </slot>
+            </v-list-tile-action-text>
+          </v-list-tile-action>
+        </v-list-tile>
+      </template>
+    </virtual-scroller>
   </v-list>
 </template>
 <script>
 import * as VList from 'vuetify/es5/components/VList';
+import { VirtualScroller } from 'vue-virtual-scroller';
 
 // @vue/component
 export default {
   name: 'List',
   components: {
     ...VList,
+    VirtualScroller,
   },
   props: {
     data: {
