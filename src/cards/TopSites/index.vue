e77e950f238b0b5b6f6ba27b477f0b51cf28d987
Alexis <alexisr245@gmail.com>
2018-09-12 23:32:23 +0200

    refactor: grid list

--- src/cards/TopSites/index.vue
+++ src/cards/TopSites/index.vue
@@ -8,3 +8,4 @@
   </v-card-text>
 </template>
 <script src="./main.js"></script>
+<style lang="scss" rel='stylesheet/scss' src="./style.scss" scoped></style>
--- src/components/GridList/index.vue
+++ src/components/GridList/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="grid-list">
     <li v-for="node in data" :key="node.name" class="node">
-      <div :title="node.name" @click="$emit('clicked', node)">
-        <i :style="{ 'background-image': `url(${node.icon})` }"/>
-      </div>
+      <a
+        :href="node.url"
+        :title="node.name"
+        :style="{ 'background-image': `url(${node.icon})` }" @click="$emit('clicked', node)"/>
     </li>
   </div>
 </template>
