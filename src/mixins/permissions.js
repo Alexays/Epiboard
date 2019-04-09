982367eb0d694f167f048929c0bb295d70918b27
Alex <alexisr245@gmail.com>
2019-04-09 10:08:13 +0200

    fix: typo

--- src/mixins/permissions.js
+++ src/mixins/permissions.js
@@ -18,7 +18,7 @@ export default {
               return granted;
             });
           }
-          throw new Error('User has refused dialog');
+          throw new Error('User has refused the dialog');
         }));
     },
   },
